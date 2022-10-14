19caffbefb17dc9f94b432d801f7fba001221bf4
reviakin <reviakindi@gmail.com>
2022-10-14 16:16:27 +0200

    add theme

--- lua/hesoyam/packer.lua
+++ lua/hesoyam/packer.lua
@@ -1,6 +1,13 @@
 return require("packer").startup(function()
     use("wbthomason/packer.nvim")
     
-    use("folke/tokyonight.nvim")
-    use("neovim/nvim-lspconfig")
+    use("folke/tokyonight.nvim") -- theme
+
+    use("neovim/nvim-lspconfig") -- lsp config
+    
+    use("nvim-treesitter/nvim-treesitter", {
+        run = ":TSUpdate"
+    })
+
+}
 end)

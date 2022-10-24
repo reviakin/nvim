568890d114cb2b9cebf9931c57b0c5547d2ff907
reviakin <reviakindi@gmail.com>
2022-10-24 18:21:43 +0200

    add telescope

--- lua/hesoyam/packer.lua
+++ lua/hesoyam/packer.lua
@@ -12,4 +12,12 @@ return require("packer").startup(function()
 
     -- Neogit
     use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+    use("nvim-lua/popup.nvim")
+    use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+
+
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -99,6 +99,16 @@ _G.packer_plugins = {
     path = "/Users/dmitry/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["popup.nvim"] = {
+    loaded = true,
+    path = "/Users/dmitry/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    url = "https://github.com/nvim-lua/popup.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/Users/dmitry/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
   ["tokyonight.nvim"] = {
     loaded = true,
     path = "/Users/dmitry/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",

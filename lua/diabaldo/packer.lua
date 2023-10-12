c858bbc89f07222966bd681d5aee59613156b26d
TheHesoyam <reviakindi@gmail.com>
2023-10-12 17:21:24 +0200

    add ts go servers; packer cmp

--- lua/diabaldo/packer.lua
+++ lua/diabaldo/packer.lua
@@ -10,12 +10,23 @@ return require('packer').startup(function(use)
   use {
        "williamboman/mason.nvim",
        "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-      "VonHeikemen/lsp-zero.nvim",
-        -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      'hrsh7th/cmp-nvim-lsp',
   }
+use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment these if you want to manage LSP servers from neovim
+    -- {'williamboman/mason.nvim'},
+    -- {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
   use 'tpope/vim-fugitive'
   use 'nvim-lualine/lualine.nvim'
 end)
